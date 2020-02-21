4c2b9375f4e29f9ccbe9bf362a6444855f0ffcbe
Alex Kiprop <kipropalex59@gmail.com>
2020-02-21 09:16:14 +0300

    create a function that returns its details

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=30)
+    count = models.IntegerField(default=0,blank=True) 
+    
+    def create_neighbourhood(self):
+        self.save()
+      
+    def delete_neighbourhood(self):
+        self.delete()
+        
+    @classmethod
+    def find_by_id(cls,id):
+        hoods = cls.objects.filter(id=id)
+        return hoods  
+    
+    def __str__(self):
+        return self.name
